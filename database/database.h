b6e974ff213fe3c4096608a587d566913c9ec9ea
ali <emami1999@gmail.com>
2019-05-09 03:51:31 +0430

    add database.h attributes

---
+++ database/database.h
@@ -0,0 +1,17 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <vector>
+#include "../models/modelHeaders.h"
+
+class Database {
+   public:
+   private:
+    std::vector<User*> users;
+    std::vector<Film*> films;
+    std::vector<Comment*> comments;
+    std::vector<Purchase*> purchases;
+    std::vector<Notification*> notifications;
+};
+
+#endif
