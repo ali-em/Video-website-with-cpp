92861e958384bcd9adc6a637d233571e34247edc
ali <emami1999@gmail.com>
2019-05-09 03:28:37 +0430

    add app attributes

---
+++ controllers/app.h
@@ -0,0 +1,15 @@
+#ifndef APP_H
+#define APP_H
+#include "../database/database.h"
+#include "../views/view.h"
+#include "input_handler.h"
+
+class App {
+   public:
+   private:
+    Database database;
+    inputHandler req;
+    View res;
+};
+
+#endif
