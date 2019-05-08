2d29295f8d616a9013c4e2477202752b8a86fd62
ali <emami1999@gmail.com>
2019-05-09 03:38:17 +0430

    add model header for easier model includes

---
+++ models/modelHeaders.h
@@ -0,0 +1,11 @@
+#ifndef MODEL_HEADERS_H
+#define MODEL_HEADERS_H
+
+#include "comment.h"
+#include "film.h"
+#include "notification.h"
+#include "publisher.h"
+#include "purchase.h"
+#include "user.h"
+
+#endif
