ae7f1730629542c4e4bd1b0d58428650c5676215
ali <emami1999@gmail.com>
2019-05-11 00:33:56 +0430

    add print films to view

--- views/view.cpp
+++ views/view.cpp
@@ -7,3 +7,18 @@ void View::send(std::string msg) {
 void View::sendError(std::exception& ex) {
     cout << ex.what() << endl;
 }
+void View::printFilms(vector<Film*>& films) {
+    int i = 1;
+    cout << "#. "
+         << "Film id" << DIVIDER
+         << "Film Name" << DIVIDER
+         << "Film Length" << DIVIDER
+         << "Film price" << DIVIDER
+         << "rate" << DIVIDER
+         << "Production Year" << DIVIDER
+         << "Film Director" << endl;
+    for (auto f : films) {
+        cout << i++ << ". "
+             << f->getInfo() << endl;
+    }
+}
--- views/view.h
+++ views/view.h
@@ -6,12 +6,13 @@
 #include <map>
 #include <string>
 #include "../models/modelHeaders.h"
-
+#define DIVIDER " | "
 class View {
    public:
     std::map<std::string, std::string> get();
     void send(std::string);
     void sendError(std::exception&);
+    void printFilms(std::vector<Film*>&);
 
    private:
 };
