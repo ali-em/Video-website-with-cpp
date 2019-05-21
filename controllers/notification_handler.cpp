77c890cfef3bbf9089b5d3f03f0314d228eaa1f2
ali <emami1999@gmail.com>
2019-05-21 16:26:26 +0430

    add notification handler

--- controllers/notification_handler.cpp
+++ controllers/notification_handler.cpp
@@ -1,10 +1,46 @@
 #include "notification_handler.h"
 using namespace std;
-NotificationHandler::NotificationHandler(Database* db, View* view, Login* _login) : DB(db), Res(view), login(_login) {}
 
-void NotificationHandler::sendReplyNotif(User* sender, User* reciever);
-void NotificationHandler::sendRegisterFilmNotif(User* sender, User* reciever);
-void NotificationHandler::sendFollowNotif(User* sender, User* reciever);
-void NotificationHandler::sendBuyNotif(User* sender, User* reciever);
-void NotificationHandler::sendRateNotif(User* sender, User* reciever);
-void NotificationHandler::sendCommentNotif(User* sender, User* reciever);
+void NotificationHandler::sendReplyNotif(User* sender, User* reciever) {
+    string msg = "Publisher " + sender->getUsername() + " with id " + to_string(sender->getId()) + " reply to your comment.";
+    Notification* notif = new Notification(msg);
+    reciever->addNotif(notif);
+}
+void NotificationHandler::sendRegisterFilmNotif(Publisher* sender) {
+    string msg = "Publisher " + sender->getUsername() + " with id " + to_string(sender->getId()) + " register new film.";
+    Notification* notif = new Notification(msg);
+    sender->sendNotifToFollowers(notif);
+}
+void NotificationHandler::sendFollowNotif(User* sender, User* reciever) {
+    string msg = "User " + sender->getUsername() + " with id " + to_string(sender->getId()) + " follow you.";
+    Notification* notif = new Notification(msg);
+    reciever->addNotif(notif);
+}
+void NotificationHandler::sendBuyNotif(User* sender, User* reciever, Film* film) {
+    string msg = "User " + sender->getUsername() + " with id " + to_string(sender->getId()) + " buy your film " + film->getName() + " with id " + to_string(film->getId()) + ".";
+    Notification* notif = new Notification(msg);
+    reciever->addNotif(notif);
+}
+void NotificationHandler::sendRateNotif(User* sender, User* reciever, Film* film) {
+    string msg = "User " + sender->getUsername() + " with id " + to_string(sender->getId()) + " rate your film " + film->getName() + " with id " + to_string(film->getId()) + ".";
+    Notification* notif = new Notification(msg);
+    reciever->addNotif(notif);
+}
+void NotificationHandler::sendCommentNotif(User* sender, User* reciever, Film* film) {
+    string msg = "User " + sender->getUsername() + " with id " + to_string(sender->getId()) + " comment on your film " + film->getName() + " with id " + to_string(film->getId()) + ".";
+    Notification* notif = new Notification(msg);
+    reciever->addNotif(notif);
+}
+void NotificationHandler::getNotifications(User* user) {
+    vector<string> notifs = user->getUnReadNotifications();
+    reverse(notifs.begin(), notifs.end());
+    View::showNotifications(notifs);
+}
+void NotificationHandler::getReadNotifications(User* user, Parameters& params) {
+    vector<string> notifs = user->getReadNotifications();
+    reverse(notifs.begin(), notifs.end());
+    vector<string> filtered;
+    for (int i = 0; i < stoi(params["limit"]) && i < notifs.size(); i++)
+        filtered.push_back(notifs[i]);
+    View::showNotifications(filtered);
+}
--- controllers/notification_handler.h
+++ controllers/notification_handler.h
@@ -1,20 +1,23 @@
 #ifndef NOTIFICATION_HANDLER_H
 #define NOTIFICATION_HANDLER_H
+#include <algorithm>
 #include <string>
 #include "../models/modelHeaders.h"
 #include "../views/view.h"
-#include "login.h"
 #include "tools.h"
-class Database;
 
 class NotificationHandler {
    public:
-    NotificationHandler(Database*, View*, Login*);
+    static void sendReplyNotif(User* sender, User* reciever);
+    static void sendRegisterFilmNotif(Publisher* sender);
+    static void sendFollowNotif(User* sender, User* reciever);
+    static void sendBuyNotif(User* sender, User* reciever, Film* film);
+    static void sendRateNotif(User* sender, User* reciever, Film* film);
+    static void sendCommentNotif(User* sender, User* reciever, Film* film);
+    static void getNotifications(User* user);
+    static void getReadNotifications(User* user, Parameters& params);
 
    private:
-    Database* DB;
-    View* Res;
-    Login* login;
 };
 
 #endif
