838e87c14bc1c74a0374ce1ed1ad867469545d8c
ali <emami1999@gmail.com>
2019-05-21 16:22:34 +0430

    fixed GET film

--- controllers/film_manager.cpp
+++ controllers/film_manager.cpp
@@ -3,17 +3,19 @@
 
 using namespace std;
 
-FilmManager::FilmManager(Database* db, View* view, Login* _login) : DB(db), Res(view), login(_login) {}
+FilmManager::FilmManager(Database* db, Login* _login) : DB(db), login(_login) {}
 
 void FilmManager::handleAddFilm(Parameters& params) {
     validateAdd(params);
     Film* newFilm = new Film(params);
     DB->addFilm(newFilm);
-    static_cast<Publisher*>(login->getCurrentUser())->addFilm(newFilm);
-    Res->send("OK");
+    Publisher* pub = static_cast<Publisher*>(login->getCurrentUser());
+    pub->addFilm(newFilm);
+    NotificationHandler::sendRegisterFilmNotif(pub);
+    View::success();
 }
 void FilmManager::validateAdd(Parameters& params) {
-    if (!Tools::isInMap(params, 6, "name", "year", "length", "price", "summary", "director"))
+    if (!Tools::checkParam(params, 6, "name", "year", "length", "price", "summary", "director"))
         throw BadRequest();
     if (!login->isLoggedIn() || !login->isLoggedIn() || !login->getCurrentUser()->isPublisher())
         throw PermissionDenied();
@@ -24,10 +26,10 @@ void FilmManager::handleEditFilm(Parameters& params) {
     validateEdit(params);
     Film* film = DB->getFilmById(stoi(params["film_id"]));
     film->edit(params);
-    Res->send("OK");
+    View::success();
 }
 void FilmManager::validateEdit(Parameters& params) {
-    if (!Tools::isInMap(params, 1, "film_id"))
+    if (!Tools::checkParam(params, 1, "film_id"))
         throw BadRequest();
     if (!login->isLoggedIn() || !login->getCurrentUser()->isPublisher())
         throw PermissionDenied();
@@ -40,11 +42,11 @@ void FilmManager::handleDeleteFilm(Parameters& params) {
     validateDelete(params);
     Film* film = DB->getFilmById(stoi(params["film_id"]));
     film->_delete();
-    Res->send("OK");
+    View::success();
 }
 
 void FilmManager::validateDelete(Parameters& params) {
-    if (!Tools::isInMap(params, 1, "film_id"))
+    if (!Tools::checkParam(params, 1, "film_id"))
         throw BadRequest();
     if (!DB->getFilmById(stoi(params["film_id"])))
         throw NotFound();
@@ -57,20 +59,24 @@ void FilmManager::validateDelete(Parameters& params) {
 void FilmManager::handleGetFilms(Parameters& params) {
     validateGet();
     User* currentUser = login->getCurrentUser();
-    if (Tools::isInMap(params, 1, "film_id")) {
+    if (Tools::isInMap(params, "film_id")) {
         Film* film = DB->getFilmById(stoi(params["film_id"]));
-
-        Res->showFilmDetails(film->getDetails(), film->getComments(), DB->getRecommendedFilms(currentUser));
+        if (film->isDeleted())
+            throw NotFound();
+        View::showFilmDetails(film->getDetails(), film->getComments(), DB->getRecommendedFilms(currentUser, film));
         return;
     }
     vector<Film*> filtered;
-    if (!currentUser->isPublisher())
-        filtered = filterFilms(params, DB->getFilms());
-    else {
-        Publisher* pub = static_cast<Publisher*>(currentUser);
-        filtered = filterFilms(params, pub->getFilms());
-    }
-    Res->printFilms(filtered);
+    filtered = filterFilms(params, DB->getFilms(), true);
+    View::printFilms(filtered);
+}
+void FilmManager::handleGetPublished(Parameters& params) {
+    validateGet();
+    User* currentUser = login->getCurrentUser();
+    vector<Film*> filtered;
+    Publisher* pub = static_cast<Publisher*>(currentUser);
+    filtered = filterFilms(params, pub->getFilms(), true);
+    View::printFilms(filtered);
 }
 
 void FilmManager::validateGet() {
@@ -78,10 +84,11 @@ void FilmManager::validateGet() {
         throw PermissionDenied();
 }
 
-vector<Film*> FilmManager::filterFilms(Parameters& params, vector<Film*> films) {
+vector<Film*> FilmManager::filterFilms(Parameters& params, vector<Film*> films, bool includeDeleted) {
     vector<Film*> result;
     for (auto f : films)
-        if (f->isMatch(params))
+        if (f->isMatch(params) &&
+            (!f->isDeleted() || includeDeleted))
             result.push_back(f);
     return result;
 }
@@ -94,11 +101,12 @@ void FilmManager::handleRate(Parameters& params) {
         film->setRate(user, stoi(params["score"]));
     else
         throw PermissionDenied();
-    Res->send("OK");
+    NotificationHandler::sendRateNotif(user, DB->getPublisherByFilmId(stoi(params["film_id"])), film);
+    View::success();
 }
 void FilmManager::handleGetPurchased(Parameters& params) {
     validateGet();
     User* user = login->getCurrentUser();
     vector<Film*> films = filterFilms(params, user->getPurchased());
-    Res->printFilms(films);
+    View::printFilms(films);
 }
--- controllers/film_manager.h
+++ controllers/film_manager.h
@@ -4,27 +4,28 @@
 #include "../models/modelHeaders.h"
 #include "../views/view.h"
 #include "login.h"
+#include "notification_handler.h"
 #include "tools.h"
 class Database;
 class FilmManager {
    public:
-    FilmManager(Database*, View*, Login*);
+    FilmManager(Database*, Login*);
     void handleAddFilm(Parameters&);
     void handleEditFilm(Parameters&);
     void handleDeleteFilm(Parameters&);
     void handleGetFilms(Parameters&);
     void handleRate(Parameters&);
     void handleGetPurchased(Parameters&);
+    void handleGetPublished(Parameters&);
 
    private:
     Database* DB;
-    View* Res;
     Login* login;
     void validateEdit(Parameters&);
     void validateAdd(Parameters&);
     void validateDelete(Parameters&);
     void validateGet();
-    std::vector<Film*> filterFilms(Parameters&, std::vector<Film*>);
+    std::vector<Film*> filterFilms(Parameters&, std::vector<Film*>, bool includeDeleted = false);
 };
 
 #endif
